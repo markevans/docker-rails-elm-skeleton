4fae84be7166c4b1857db9f4d2e99f77c001ef44
Mark Evans <markevans3000@gmail.com>
2017-06-28 14:09:13 +0100

    got local webpack-dev-server working on docker by hacking the host to be the same as the docker host

--- web/app/helpers/application_helper.rb
+++ web/app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def js_pack_tag_same_host(name)
+    # Using the same host makes it easier to work with Docker
+    javascript_pack_tag(name).sub(/\d+\.\d+\.\d+\.\d+/, request.host).html_safe
+  end
+
 end
